031a233930d418c0dbb5b640375764e088ab8c91
dmzimmerman13 <fondyzim@hotmail.com>
2016-02-18 16:34:30 -0600

    Added Start Screen and First Fragment

---
+++ app/src/main/java/com/test/david/homeworkone/PlayActivity.java
@@ -0,0 +1,23 @@
+package com.test.david.homeworkone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+
+public class PlayActivity extends AppCompatActivity implements PlayFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new PlayFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void onFragmentInteraction(Uri uri){
+
+    }
+}
